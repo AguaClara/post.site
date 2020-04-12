e2a433b83503bf62cc0f564be476eab869304a87
Giancarlo Pacenza <giancarlopacenza@dhcp-hol-6322.redrover.cornell.edu>
2020-04-12 09:14:32 -0400

    added firebase config to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  title: "post-visualize-devolopment",
+  firebaseConfig: {
+    apiKey: "AIzaSyAXe0rjDTHgcoUSsP9u5NtiwNf92VL_oI4",
+    authDomain: "post-visualize-development.firebaseapp.com",
+    databaseURL: "https://post-visualize-development.firebaseio.com",
+    projectId: "post-visualize-development",
+    storageBucket: "post-visualize-development.appspot.com",
+    messagingSenderId: "114314763008"
+  }
 };
